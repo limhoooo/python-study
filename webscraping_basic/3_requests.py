4fd691dbb202e1c0d283165bdc44c980dbc1552f
lim ho <dlagh123@gmail.com>
2021-07-05 19:36:44 +0900

    .

---
+++ webscraping_basic/3_requests.py
@@ -0,0 +1,22 @@
+import requests
+# res = requests.get("http://naver.com")
+
+
+# print("응답코드 : ", res.status_code)  # 200이면 정상
+
+# if res.status_code == requests.codes.ok:
+#     print("정상입니다.")
+# else:
+#     print("문제가 생겼습니다. [에러코드 ", res.status_code, "]")
+
+res = requests.get("http://github.com/limhoooo")
+res.raise_for_status()  # 문제가 생겼을지 여기서 멈춤
+print("웹 스크래핑을 진행합니다.")
+
+# print(res.text) # text 로 다 갖고오기
+print(len(res.text))  # text length 갖고오기
+
+# html 파일로 갖고올수있음.. 와 개쩐다...
+# "w"는 쓰기모드 : write
+with open("myhompage.html", "w", encoding="utf8") as f:
+    f.write(res.text)
