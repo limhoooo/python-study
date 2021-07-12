9e2f7889c0559282106fe4b375515f233a807edd
lim ho <dlagh123@gmail.com>
2021-07-12 22:26:20 +0900

    .

--- webscraping_basic/9_bs4_coupang.py
+++ webscraping_basic/9_bs4_coupang.py
@@ -1,8 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=&backgroundColor="
-res = requests.get(url)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+res = requests.get(url, headers=headers)
 res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
-print('sss')
+items = soup.find_all("li", attrs={"class": re.compile("^search-product")})
+
+for item in items:
+    name = item.find("div", attrs={"class": "name"}).get_text()  # 제품명
+    price = item.find(
+        "string", attrs={"class": "price-value"})  # 가격
+
+    print("name : ", name)
+    print("price : ", price)
